b94cc167fee691fe6f14664cebc0a276589e9e02
AnthonyA490 <113041244+AnthonyA490@users.noreply.github.com>
2022-09-21 17:15:23 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= x; y++) {
+            led.plot(x, y)
+            basic.pause(200)
+            led.plot(y, x)
+        }
+    }
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= x; y++) {
+            led.unplot(x, y)
+            basic.pause(200)
+            led.unplot(y, x)
+        }
+    }
 })
+basic.showIcon(IconNames.Angry)
